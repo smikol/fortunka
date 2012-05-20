29257db7c2ababe57a8410a6f47e162c7e7ae522
root <root@seba-VirtualBox.(none)>
2012-05-20 13:04:31 +0200

    first init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fortunka::Application.config.secret_token = 'c5042a89959fada987c9ac50867ad7fcdafde2e39acf2f1b64f50e1a387314709a8a82d6cb03ade51b6957afeed7eda77d14ae63828a60832264dcb78cefeae9'
